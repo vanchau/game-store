dbfea23927dc22fb67c47c2c714db206cd55d164
vchau <van.chau@aalto.fi>
2020-01-25 23:27:02 +0200

    modified game path name

--- gamestore/urls.py
+++ gamestore/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('<int:pk>/', views.GameView.as_view(), name='game')
+    path('games/<int:pk>/', views.GameView.as_view(), name='game')
 ]
