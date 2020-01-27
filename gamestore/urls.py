05ec8770487fc77b9b8fc5890c2159ca3eb4489d
Minna <minna.maaria.laitinen@gmail.com>
2020-01-27 23:10:03 +0200

    'Add games' feature

--- gamestore/urls.py
+++ gamestore/urls.py
@@ -6,5 +6,6 @@ app_name = 'games'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('<int:pk>/', views.GameView.as_view(), name='game'),
-    path('delete/<int:pk>/', views.DeleteGame.as_view(), name="delete"),
+    path("new/", views.PublishGame.as_view(), name='publish'),
+    path('delete/<int:pk>/', views.DeleteGame.as_view(), name='delete'),
 ]
